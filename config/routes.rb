364b01a1e6813381aff8227a28c49c0736ab66cf
tsized <timsized@yahoo.com>
2014-05-22 08:35:19 -0400

    Add working links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  match '/static_pages/',        to: 'static_pages#home',    via: 'get'
+  match '/static_pages/help',    to: 'static_pages#help',    via: 'get'
+  match '/static_pages/about',   to: 'static_pages#about',   via: 'get'
+  match '/static_pages/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
