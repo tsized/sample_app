c178e63694fe40edc759159c5c58d94aa7c878e3
tsized <timsized@yahoo.com>
2014-05-23 05:24:33 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  match '/static_pages/',        to: 'static_pages#home',    via: 'get'
-  match '/static_pages/help',    to: 'static_pages#help',    via: 'get'
-  match '/static_pages/about',   to: 'static_pages#about',   via: 'get'
-  match '/static_pages/contact', to: 'static_pages#contact', via: 'get'
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
