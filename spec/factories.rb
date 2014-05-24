ba4a0481edfb37692c885af412307036cb604c0c
tsized <timsized@yahoo.com>
2014-05-24 10:03:54 -0400

    Make profile page layout

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Tim Sizemore"
+        email "timsized@yahoo.com"
+        password "donnie88"
+        password_confirmation "donnie88"
+    end
+end
